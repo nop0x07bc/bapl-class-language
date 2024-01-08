6d3afdd50f98f9315d525a82da05edf2b9a8911e
Peter Niitenberg <peter.niitenberg@gacrux.technology>
2024-01-08 22:03:21 +0100

    Reduce loop time for exercise 03 (save github compute).

--- lesson-6/exercises/exercises_03.lua
+++ lesson-6/exercises/exercises_03.lua
@@ -15,22 +15,22 @@ TestMachine = {}
         machine:setTrace(false)
 
         -- GC measurment
-        program = compiler.compile("n = 1; x = new [1000000]; while n <= 1000000 { x[n] = n * 2;  n = n + 1}; return x[500000];")
+        program = compiler.compile("n = 1; x = new [100000]; while n <= 100000 { x[n] = n * 2;  n = n + 1}; return x[50000];")
         machine:load(program)
 
         collectgarbage("collect")
         gc_size_before = collectgarbage("count")
         print("Allocated before: ", gc_size_before, "kB")
         machine:run()
-        lu.assertEquals(machine:tos(), 1000000)
+        lu.assertEquals(machine:tos(), 100000)
 
         collectgarbage("collect")
         gc_size_after = collectgarbage("count")
         print("Allocated after:  ", gc_size_after, "kB")
-        lu.assertNotAlmostEquals(gc_size_before/gc_size_after, 1, 0.9)
+        lu.assertNotAlmostEquals(gc_size_before/gc_size_after, 1, 0.8)
 
 
-        program = compiler.compile("n = 1; x = new [1000000]; while n <= 1000000 { x[n] = n * 2;  n = n + 1}; x = 10; return x;")
+        program = compiler.compile("n = 1; x = new [100000]; while n <= 100000 { x[n] = n * 2;  n = n + 1}; x = 10; return x;")
         machine:load(program)
 
         collectgarbage("collect")
@@ -42,7 +42,7 @@ TestMachine = {}
         collectgarbage("collect")
         gc_size_after = collectgarbage("count")
         print("Allocated after:  ", gc_size_after, "kB")
-        lu.assertAlmostEquals(gc_size_before/gc_size_after, 1, 0.1)
+        lu.assertAlmostEquals(gc_size_before/gc_size_after, 1, 0.2)
     end
 os.exit(lu.LuaUnit:run())
 
